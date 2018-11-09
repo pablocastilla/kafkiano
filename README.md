91449f7099f1a4a2bc350105e09a89faa6b0f2bc
Pablo <pablo.castilla@gmail.com>
2018-11-09 18:53:47 +0100

    connected to confluent in docker!

---
+++ README.md
@@ -0,0 +1,15 @@
+https://github.com/confluentinc/kafka-streams-examples/tree/3.3.1-post/src/main/java/io/confluent/examples/streams/microservices
+https://docs.confluent.io/current/installation/docker/docs/installation/single-node-client.html
+
+assign 	127.0.0.1		kafka in host
+
+docker network create confluent
+docker run -d --net=confluent --name=zookeeper -e ZOOKEEPER_CLIENT_PORT=2181 confluentinc/cp-zookeeper:5.0.1
+docker run -d --net=confluent --name=kafka -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 confluentinc/cp-kafka:5.0.1
+
+create topic
+docker run --net=confluent --rm confluentinc/cp-kafka:5.0.1 kafka-topics --create --topic OrdersCommands --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:2181
+docker run --net=confluent --rm confluentinc/cp-kafka:5.0.1 bash -c "seq 42 | kafka-console-producer --request-required-acks 1 --broker-list kafka:9092 --topic OrdersCommands && echo 'Produced 42 messages.'"
+
+END
+docker-compose down 
---
+++ Services/OrdersService/Program.cs
@@ -0,0 +1,101 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace OrdersService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Started consumer, Ctrl-C to stop consuming");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+
+            Run_Consume(new List<string>() {"foo"}, cts.Token);
+        }
+
+
+        public static void Run_Consume(List<string> topics, CancellationToken cancellationToken)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "127.0.0.1:9092",
+                GroupId = "csharp-consumer",
+                EnableAutoCommit = false,
+                StatisticsIntervalMs = 5000,
+                SessionTimeoutMs = 6000,
+                AutoOffsetReset = AutoOffsetResetType.Earliest
+            };
+
+            const int commitPeriod = 5;
+
+            using (var consumer = new Consumer<Ignore, string>(config))
+            {
+                // Note: All event handlers are called on the main .Consume thread.
+
+                // Raised when the consumer has been notified of a new assignment set.
+                // You can use this event to perform actions such as retrieving offsets
+                // from an external source / manually setting start offsets using
+                // the Assign method. You can even call Assign with a different set of
+                // partitions than those in the assignment. If you do not call Assign
+                // in a handler of this event, the consumer will be automatically
+                // assigned to the partitions of the assignment set and consumption
+                // will start from last committed offsets or in accordance with
+                // the auto.offset.reset configuration parameter for partitions where
+                // there is no committed offset.
+                consumer.OnPartitionsAssigned += (_, partitions)
+                    => Console.WriteLine($"Assigned partitions: [{string.Join(", ", partitions)}], member id: {consumer.MemberId}");
+
+                // Raised when the consumer's current assignment set has been revoked.
+                consumer.OnPartitionsRevoked += (_, partitions)
+                    => Console.WriteLine($"Revoked partitions: [{string.Join(", ", partitions)}]");
+
+                consumer.OnPartitionEOF += (_, tpo)
+                    => Console.WriteLine($"Reached end of topic {tpo.Topic} partition {tpo.Partition}, next message will be at offset {tpo.Offset}");
+
+                consumer.OnError += (_, e)
+                    => Console.WriteLine($"Error: {e.Reason}");
+
+                consumer.OnStatistics += (_, json)
+                    => Console.WriteLine($"Statistics: {json}");
+
+                consumer.Subscribe(topics);
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume(cancellationToken);
+                        Console.WriteLine($"Topic: {consumeResult.Topic} Partition: {consumeResult.Partition} Offset: {consumeResult.Offset} {consumeResult.Value}");
+
+                        if (consumeResult.Offset % commitPeriod == 0)
+                        {
+                            // The Commit method sends a "commit offsets" request to the Kafka
+                            // cluster and synchronously waits for the response. This is very
+                            // slow compared to the rate at which the consumer is capable of
+                            // consuming messages. A high performance application will typically
+                            // commit offsets relatively infrequently and be designed handle
+                            // duplicate messages in the event of failure.
+                            var committedOffsets = consumer.Commit(consumeResult);
+                            Console.WriteLine($"Committed offset: {committedOffsets}");
+                        }
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Consume error: {e.Error}");
+                    }
+                }
+
+                consumer.Close();
+            }
+        }
+
+    }
+}
